35fa9bffd14b25eacf5eb8346ec773ea6c527612
haruhadj <michaelfernandezskie@gmail.com>
2024-08-22 16:30:32 +0800

    add rss feeds

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,10 +1,31 @@
 export const feeds = [
-  // Daring Fireball
-  'https://daringfireball.net/feeds/main',
+  // Sono Bisque Doll wa Koi wo suru - Releases on MangaUpdates
+  "https://api.mangaupdates.com/v1/series/13952982910/rss",
 
-  // JakeArchibald.com
-  'https://jakearchibald.com/posts.rss',
+  // Vinland Saga
+  "https://api.mangaupdates.com/v1/series/27728982867/rss",
 
-  // Appjeniksaan
-  'https://appjeniksaan.nl/feed.xml',
+  // Oshi no Ko
+  "https://api.mangaupdates.com/v1/series/17997087747/rss",
+
+  // Boku no Kokoro no Yabai Yatsu
+  "https://api.mangaupdates.com/v1/series/9143777514/rss",
+
+  // Kimi no Koto ga Dai Dai Dai Dai Daisuki na 100-nin no Kanojo
+  "https://api.mangaupdates.com/v1/series/23862878832/rss",
+
+  // Uzaki-chan wa Asobitai
+  "https://api.mangaupdates.com/v1/series/60675588667/rss",
+
+  // Grand Blue - Releases on MangaUpdates
+  "https://api.mangaupdates.com/v1/series/65672018770/rss",
+
+  // Nyaa - "oshi no ko nandesuka 2160p 265" - Torrent File RSS
+  "https://nyaa.si/?page=rss&q=oshi+no+ko+nandesuka+2160p+265&c=1_2&f=0",
+
+  // Nyaa - "gimai seikatsu 1080p subsplease" - Torrent File RSS
+  "https://nyaa.si/?page=rss&q=gimai+seikatsu+1080p+subsplease&c=1_2&f=0",
+
+  // Nyaa - "Giji Harem 1080p subsplease" - Torrent File RSS
+  "https://nyaa.si/?page=rss&q=Giji+Harem+1080p+subsplease&c=1_2&f=0",
 ]
