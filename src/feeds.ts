a533967d01d9011a1fe442f947a6779817b64cf7
haruhadj <michaelfernandezskie@gmail.com>
2024-10-26 21:37:17 +0800

    update feeds

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,52 +1,52 @@
 export const feeds = [
   // Sono Bisque Doll wa Koi wo suru - Releases on MangaSee
-  "https://mangasee123.com/rss/Sono-Bisque-Doll-Wa-Koi-Wo-Suru.xml",
+  'https://mangasee123.com/rss/Sono-Bisque-Doll-Wa-Koi-Wo-Suru.xml',
 
   // Vinland Saga - Releases on MangaSee
-  "https://mangasee123.com/rss/Vinland-Saga.xml",
+  'https://mangasee123.com/rss/Vinland-Saga.xml',
 
   // Oshi no Ko - Releases on MangaSee
-  "https://mangasee123.com/rss/Oshi-no-Ko.xml",
+  'https://mangasee123.com/rss/Oshi-no-Ko.xml',
 
   // Boku no Kokoro no Yabai Yatsu - Releases on MangaSee
-  "https://mangasee123.com/rss/Boku-no-Kokoro-no-Yabai-Yatsu.xml",
+  'https://mangasee123.com/rss/Boku-no-Kokoro-no-Yabai-Yatsu.xml',
 
   // Uzaki-chan wa Asobitai - Releases on MangaSee
-  "https://mangasee123.com/rss/Uzaki-chan-Wa-Asobitai.xml",
+  'https://mangasee123.com/rss/Uzaki-chan-Wa-Asobitai.xml',
 
   // Grand Blue - Releases on MangaSee
-  "https://mangasee123.com/rss/Grand-Blue.xml",
+  'https://mangasee123.com/rss/Grand-Blue.xml',
 
   // Fuufu Ijou, Koibito Miman - Releases on MangaSee
-  "https://mangasee123.com/rss/Fuufu-Ijou-Koibito-Miman.xml",
+  'https://mangasee123.com/rss/Fuufu-Ijou-Koibito-Miman.xml',
 
   // Soredemo Ayumu wa Yosetekuru - Releases on MangaSee
-  "https://mangasee123.com/rss/Soredemo-Ayumu-wa-Yosetekuru.xml",
+  'https://mangasee123.com/rss/Soredemo-Ayumu-wa-Yosetekuru.xml',
 
   // The Hero Took Everything from Me, So I Partied with the Hero's Mother - Releases on MangaSee
-  "https://mangasee123.com/rss/The-Hero-Took-Everything-from-Me-So-I-Partied-with-the-Heros-Mother.xml",
+  'https://mangasee123.com/rss/The-Hero-Took-Everything-from-Me-So-I-Partied-with-the-Heros-Mother.xml',
 
   // Parallel Paradise - Releases on MangaSee
-  "https://mangasee123.com/rss/Parallel-Paradise.xml",
+  'https://mangasee123.com/rss/Parallel-Paradise.xml',
 
   // Kakkou no Iinazuke - Releases on MangaSee
-  "https://mangasee123.com/rss/Kakkou-no-Iinazuke.xml",
+  'https://mangasee123.com/rss/Kakkou-no-Iinazuke.xml',
 
   // Tonikaku Kawaii - Releases on MangaSee
-  "https://mangasee123.com/rss/Tonikaku-Kawaii.xml",
+  'https://mangasee123.com/rss/Tonikaku-Kawaii.xml',
 
   // One Punch Man - Releases on MangaSee
-  "https://mangasee123.com/rss/Onepunch-Man.xml",
+  'https://mangasee123.com/rss/Onepunch-Man.xml',
 
   // Jujutsu Kaisen - Releases on MangaSee
-  "https://mangasee123.com/rss/Jujutsu-Kaisen.xml",
+  'https://mangasee123.com/rss/Jujutsu-Kaisen.xml',
 
   // Sousou no Frieren - Releases on MangaSee
-  "https://mangasee123.com/rss/Sousou-no-Frieren.xml",
+  'https://mangasee123.com/rss/Sousou-no-Frieren.xml',
 
   // The 100 Girlfriends Who Really Really Really Really Really Love You - Releases on MangaSee
-  "https://mangasee123.com/rss/The-100-Girlfriends-Who-Really-Really-Really-Really-Really-Love-You.xml",
+  'https://mangasee123.com/rss/The-100-Girlfriends-Who-Really-Really-Really-Really-Really-Love-You.xml',
 
   // Kaoru Hana wa Rin to Saku - Releases on MangaSee
-  "https://mangasee123.com/rss/Kaoru-Hana-wa-Rin-to-Saku.xml",
+  'https://mangasee123.com/rss/Kaoru-Hana-wa-Rin-to-Saku.xml',
 ];
